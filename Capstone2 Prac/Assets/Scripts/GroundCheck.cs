78212f8f4cbbc1ae0555a2a444012b364f48bd16
Steve <frombs@rpi.edu>
2020-09-19 15:19:48 -0400

    Groundcheck using raycast, tag changes

--- Capstone2 Prac/Assets/Scripts/GroundCheck.cs
+++ Capstone2 Prac/Assets/Scripts/GroundCheck.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public CharacterMovement player;
+    int layerMask;
+    RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        layerMask = 1 << 12;
     }
 
     // Update is called once per frame
@@ -17,20 +19,38 @@ public class GroundCheck : MonoBehaviour
         
     }
 
-    private void OnTriggerStay(Collider other)
+    private void FixedUpdate()
     {
-        if (other.gameObject.tag == "Ground" && !player.IsGrounded())
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down),out hit, 0.5f, layerMask))
         {
-            player.Ground();
+            if (!player.IsGrounded())
+            {
+                player.Ground();
+            }
+        }
+        else
+        {
+            if (player.IsGrounded())
+            {
+                player.Unground();
+            }
         }
     }
 
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Ground" && !player.IsGrounded())
+    //    {
+    //        player.Ground();
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            player.Unground();
-        }
-    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Ground")
+    //    {
+    //        player.Unground();
+    //    }
+    //}
 }
