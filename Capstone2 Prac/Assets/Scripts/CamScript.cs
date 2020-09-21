b5e3d1d5723fb14f041d27b53eb77f3590168854
Steve <frombs@rpi.edu>
2020-09-20 23:01:14 -0400

    Worked on camera movements, added blockout model

--- Capstone2 Prac/Assets/Scripts/CamScript.cs
+++ Capstone2 Prac/Assets/Scripts/CamScript.cs
@@ -10,29 +10,49 @@ public class CamScript : MonoBehaviour
     public float smooth;
     public float fallSmooth;
     public float yOffset;
+    public float lookDown;
     float yPos;
     float yDist;
+    float currentYOffset;
     // Start is called before the first frame update
     void Start()
     {
         yPos = player.position.y;
+        currentYOffset = yOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        followPos = new Vector3(player.position.x, yPos + yOffset, transform.position.z);
+        //followPos = new Vector3(player.position.x, yPos + yOffset, transform.position.z);
         //followPos = new Vector3(player.position.x, player.position.y + yOffset, transform.position.z);
+        //followPos = new Vector3(player.position.x, player.position.y + currentYOffset, transform.position.z);
+        followPos = new Vector3(player.position.x, yPos + currentYOffset, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, followPos, smooth);
         yDist = player.position.y - yPos;
-        if ((yDist >= 5.5f && cm.IsGrounded())|| yDist < 0f)
+        if ((yDist >= 5f && cm.IsGrounded())|| yDist < 0f)
         {
             MoveCamY();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(WaitDown());
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            StopAllCoroutines();
+            currentYOffset = yOffset;
         }
     }
 
     public void MoveCamY()
     {
         yPos = player.position.y;
+    }
+
+    IEnumerator WaitDown()
+    {
+        yield return new WaitForSeconds(0.25f);
+        currentYOffset = lookDown;
     }
 }
