cc85a7524816452d79fee1b2e933c71d1da6ff52
Laura <laura.mastracchio18@gmail.com>
2020-09-26 16:44:48 -0400

    Gravity now includes mouse movement

--- Capstone2 Prac/Assets/Scripts/ChangeGravity.cs
+++ Capstone2 Prac/Assets/Scripts/ChangeGravity.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class ChangeGravity : MonoBehaviour
 {
+    public Vector3 lastPos;
+    bool left = false;
     public float gravOffsetX = .5f;
     public float gravOffsetY = .5f;
     public Vector3 gravityVec = new Vector3 (0f, -9.8f, 0f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastPos = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0)){
+            Debug.Log(Input.mousePosition);
+            if (Input.mousePosition.x == lastPos.x){
+                if (left){
+                    transform.Rotate(0, 0, -1.5f);
+                }
+                else{
+                    transform.Rotate(0, 0, 1.5f);
+                }
+            }
+            if (Input.mousePosition.x > lastPos.x){
+                transform.Rotate(0, 0, 1.5f);
+                left = false;
+            }
+            if (Input.mousePosition.x < lastPos.x){
+                transform.Rotate(0, 0, -1.5f);
+                left = true;
+            }
+        }
         //Physics.gravity = new Vector3(-1f, 0f, 0f);
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -43,6 +64,7 @@ public class ChangeGravity : MonoBehaviour
             //Physics.gravity = -1f * transform.up * Physics.gravity.magnitude;
             Debug.Log("Neg");
         }
+        lastPos = Input.mousePosition;
         Physics.gravity = -1f * transform.up.normalized * Physics.gravity.magnitude;
         Debug.DrawRay(transform.position, Physics.gravity,Color.blue);
     }
