f25b3da2908a971e2ab212c86503d555eddbf8b7
Laura <laura.mastracchio18@gmail.com>
2020-11-19 16:42:03 -0500

    Title menu

---
+++ Capstone2 Prac/Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    //public Canvas title;
+    public Button quitButton;
+    public Button startButton;
+    public Button options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("CityLevel", LoadSceneMode.Single);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void ShowOptions(){
+        
+    }
+}
